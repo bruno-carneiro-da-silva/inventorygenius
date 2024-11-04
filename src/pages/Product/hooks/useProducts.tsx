f2779b48bc1958d0756a6c17a6bac1c88bb4fe3e
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-11-04 14:23:25 -0300

    :boom: fix: come on vercel, build it up!

--- src/pages/Product/hooks/useProducts.tsx
+++ src/pages/Product/hooks/useProducts.tsx
@@ -5,142 +5,149 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { AxiosError } from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
-import * as yup from 'yup'
-import { useCreateProduct, useUpdateProduct } from '@/queries/product'
+import * as yup from "yup";
+import { useCreateProduct, useUpdateProduct } from "@/queries/product";
 import { fileToBase64 } from "@/utils/functions";
 import { ProductResponse } from "@/queries/product/types";
 
 type FormValues = {
-    name: string;
-    description: string;
-    price: string;
-    qtd: string;
-    // size: string;
-    minStock: string;
-    capacity: string;
-    categoryId: string;
+  name: string;
+  description: string;
+  price: string;
+  qtd: string;
+  // size: string;
+  minStock: string;
+  capacity: string;
+  categoryId: string;
 };
 
 const schema: yup.ObjectSchema<FormValues> = yup.object({
-    id: yup.string().nullable(),
-    name: yup.string().required("Nome é obrigatório"),
-    description: yup.string().required("Descrição é obrigatória"),
-    price: yup.string().required("Preço é obrigatório"),
-    qtd: yup.string().required("Estoque é obrigatório"),
-    // size: yup.string().required("Tamanho é obrigatório"),
-    minStock: yup.string().required("Estoque mínimo é obrigatório"),
-    capacity: yup.string().required("Estoque máximo é obrigatório"),
-    categoryId: yup.string().required("Categoria é obrigatória"),
+  id: yup.string().nullable(),
+  name: yup.string().required("Nome é obrigatório"),
+  description: yup.string().required("Descrição é obrigatória"),
+  price: yup.string().required("Preço é obrigatório"),
+  qtd: yup.string().required("Estoque é obrigatório"),
+  // size: yup.string().required("Tamanho é obrigatório"),
+  minStock: yup.string().required("Estoque mínimo é obrigatório"),
+  capacity: yup.string().required("Estoque máximo é obrigatório"),
+  categoryId: yup.string().required("Categoria é obrigatória"),
 });
 
 interface UseProductsProps {
-    editProduct?: ProductResponse
-    onClose: () => void
+  editProduct?: ProductResponse;
+  onClose: () => void;
 }
 
 export function useProducts({ editProduct, onClose }: UseProductsProps) {
-    const methods = useForm({
-        resolver: yupResolver(schema),
-    });
+  const methods = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const { data: categories } = useListCategories();
-    const { setCategories, selectCategory } = useCategoryStore();
-    const { mutateAsync: createProduct } = useCreateProduct()
-    const { mutateAsync: updateProduct } = useUpdateProduct()
+  const { data: categories } = useListCategories();
+  const { setCategories, selectCategory } = useCategoryStore();
+  const { mutateAsync: createProduct } = useCreateProduct();
+  const { mutateAsync: updateProduct } = useUpdateProduct();
 
-    const [isLoading, setIsLoading] = React.useState(false)
-    const [file, setFile] = React.useState<File | null>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [file, setFile] = React.useState<File | null>(null);
 
-    const onSubmit = async (formData: FormValues) => {
-        if (!file && !editProduct) return showErrorToast('Foto obrigatória!')
-        setIsLoading(true);
+  const onSubmit = async (formData: FormValues) => {
+    if (!file && !editProduct) return showErrorToast("Foto obrigatória!");
+    setIsLoading(true);
 
-        const photoBase64 = file ? await fileToBase64(file) : editProduct?.photos?.[0]?.base64!
+    const photoBase64 = file
+      ? await fileToBase64(file)
+      : editProduct?.photos?.[0]?.base64!;
 
-        const finalPayload = {
-            name: formData.name,
-            capacity: Number(formData.capacity),
-            categoryId: formData.categoryId,
-            description: formData.description,
-            minStock: Number(formData.minStock),
-            price: Number(formData.price),
-            qtd: Number(formData.qtd),
-            // size: formData.size,
-            photos: [photoBase64]
-        }
+    const finalPayload = {
+      name: formData.name,
+      capacity: Number(formData.capacity),
+      categoryId: formData.categoryId,
+      description: formData.description,
+      minStock: Number(formData.minStock),
+      price: Number(formData.price),
+      qtd: Number(formData.qtd),
+      // size: formData.size,
+      photos: [photoBase64],
+    };
 
-        try {
-            var data
-            if (editProduct) {
-                data = await updateProduct({ id: editProduct.id, ...finalPayload })
-                showSuccessToast("Product updated successfully");
-            } else {
-                data = await createProduct(finalPayload)
-                showSuccessToast("Product created successfully");
-            }
-            onClose();
-            // if (onSaved && data) {
-            //   onSaved(data.data);
-            // }
-        } catch (errors) {
-            const errorMessage =
-                (errors as AxiosError<{ error: string }>)?.response?.data?.error || "An error occurred";
-            showErrorToast(errorMessage);
-        } finally {
-            setIsLoading(false)
-        }
+    try {
+      let data;
+      if (editProduct) {
+        data = await updateProduct({ id: editProduct.id, ...finalPayload });
+        showSuccessToast("Product updated successfully");
+      } else {
+        data = await createProduct(finalPayload);
+        showSuccessToast("Product created successfully");
+      }
+      onClose();
+      // if (onSaved && data) {
+      //   onSaved(data.data);
+      // }
+    } catch (errors) {
+      const errorMessage =
+        (errors as AxiosError<{ error: string }>)?.response?.data?.error ||
+        "An error occurred";
+      showErrorToast(errorMessage);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    React.useEffect(() => {
-        if (categories) {
-            setCategories(categories);
-        }
-    }, [categories, setCategories]);
-
-    const categoryOptions =
-        categories?.map((category) => ({
-            value: category.id,
-            label: category.name,
-        })) || [];
-
-    const [isCategoryModalOpen, setIsCategoryModalOpen] = React.useState(false);
-
-    const handleCreateCategory = () => {
-        setIsCategoryModalOpen(!isCategoryModalOpen);
-    };
-
-    const handleCategoryChange = (value: string) => {
-        selectCategory(value);
-        methods.setValue('categoryId', value)
-    };
-
-    const updateFile = (file: File) => {
-        setFile(file);
-    };
-
-    React.useEffect(() => {
-        if (editProduct) {
-            methods.reset({
-                categoryId: editProduct.category?.id,
-                name: editProduct.name,
-                description: editProduct.description,
-                price: String(editProduct.price),
-                minStock: editProduct.stock?.minStock ? String(editProduct.stock.minStock) : '',
-                capacity: editProduct.stock?.capacity ? String(editProduct.stock.capacity) : '',
-                qtd: editProduct.stock?.qtd ? String(editProduct.stock.qtd) : '',
-            })
-            handleCategoryChange(editProduct.category?.id)
-        }
-    }, [editProduct])
-
-    return {
-        methods,
-        isLoading,
-        onSubmit,
-        categoryOptions,
-        isCategoryModalOpen,
-        handleCreateCategory,
-        handleCategoryChange,
-        updateFile,
+  React.useEffect(() => {
+    if (categories) {
+      setCategories(categories);
     }
+  }, [categories, setCategories]);
+
+  const categoryOptions =
+    categories?.map((category) => ({
+      value: category.id,
+      label: category.name,
+    })) || [];
+
+  const [isCategoryModalOpen, setIsCategoryModalOpen] = React.useState(false);
+
+  const handleCreateCategory = () => {
+    setIsCategoryModalOpen(!isCategoryModalOpen);
+  };
+
+  const handleCategoryChange = (value: string) => {
+    selectCategory(value);
+    methods.setValue("categoryId", value);
+  };
+
+  const updateFile = (file: File) => {
+    setFile(file);
+  };
+
+  React.useEffect(() => {
+    if (editProduct) {
+      methods.reset({
+        categoryId: editProduct.category?.id,
+        name: editProduct.name,
+        description: editProduct.description,
+        price: String(editProduct.price),
+        minStock: editProduct.stock?.minStock
+          ? String(editProduct.stock.minStock)
+          : "",
+        capacity: editProduct.stock?.capacity
+          ? String(editProduct.stock.capacity)
+          : "",
+        qtd: editProduct.stock?.qtd ? String(editProduct.stock.qtd) : "",
+      });
+      handleCategoryChange(editProduct.category?.id);
+    }
+  }, [editProduct]);
+
+  return {
+    methods,
+    isLoading,
+    onSubmit,
+    categoryOptions,
+    isCategoryModalOpen,
+    handleCreateCategory,
+    handleCategoryChange,
+    updateFile,
+  };
 }
