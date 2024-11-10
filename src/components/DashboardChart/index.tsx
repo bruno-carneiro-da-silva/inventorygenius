ed6218daa02e6645307146e7cdbe99b8b92ee31c
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-11-10 19:27:27 -0300

    :boom: fix: minor fix

--- src/components/DashboardChart/index.tsx
+++ src/components/DashboardChart/index.tsx
@@ -12,23 +12,20 @@ interface ChartProps {
 }
 
 export default function DashboardChart({ data }: ChartProps) {
-  // Prepara os dados agrupando por mês
   const monthlyData = data.reduce(
     (acc, sale) => {
       sale.sales.forEach((saleItem) => {
         const month = new Date(saleItem.soldItems[0]?.createdAt).getMonth();
 
-        // Verifica se o mês já está no acumulador
         if (!acc[month]) {
           acc[month] = {
             totalSales: 0,
-            totalQuantity: 0, // Novo campo para a quantidade total
+            totalQuantity: 0,
           };
         }
 
-        // Atualizando os dados de vendas mensais
-        acc[month].totalSales += saleItem.totalPrice; // Atualizando o totalSales corretamente
-        acc[month].totalQuantity += saleItem.soldItems.length; // Atualizando a quantidade total
+        acc[month].totalSales += saleItem.totalPrice;
+        acc[month].totalQuantity += saleItem.soldItems.length;
       });
       return acc;
     },
@@ -36,22 +33,13 @@ export default function DashboardChart({ data }: ChartProps) {
       .fill(null)
       .map(() => ({
         totalSales: 0,
-        totalQuantity: 0, // Inicializa com 0
+        totalQuantity: 0,
       }))
   );
 
-  // Agora você tem os totais de faturamento e quantidade por mês
-  const revenueData = monthlyData.map((item) => item.totalSales); // Faturamento total por mês
-  const quantityData = monthlyData.map((item) => item.totalQuantity); // Quantidade total de vendas por mês
 
-  console.log(
-    "monthlyData",
-    monthlyData,
-    "revenueData",
-    revenueData,
-    "quantityData",
-    quantityData
-  );
+  const revenueData = monthlyData.map((item) => item.totalSales);
+  const quantityData = monthlyData.map((item) => item.totalQuantity);
 
   return (
     <Box sx={{ width: "100%", maxWidth: "90%", margin: "auto" }}>
